add5da2fb16f317baabe383a3e9db322063c9620
Seda <sedanurgfb6671@hotmail.com>
2023-07-07 09:42:38 +0300

    fixed app

--- commands/math/ping.js
+++ commands/math/ping.js
@@ -1,10 +1,10 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('SELAAAAAAAAAAAAAAM');
-	},
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+  async execute(interaction) {
+    await interaction.reply("SELAAAAAAAAAAAAAAMs");
+  },
 };
